d32725eea94d0d4db385235ead09c2be30a00370
codecademy <ccuser@codecademy.com>
2019-06-05 21:38:38 +0000

    Added function to generate message for traveler and present attractions they might be interested in.

--- script.py
+++ script.py
@@ -55,3 +55,17 @@ def find_attractions(destination, interests):
 la_arts = find_attractions("Los Angeles, USA", ["art"] )
 print(la_arts)
 
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+  interests_string = "Hi " 
+  interests_string += traveler[0] 
+  interests_string += ", we think you'll like these places around " + traveler_destination + ": "
+  for attraction in traveler_attractions:
+    interests_string += "the " + attraction
+    interests_string += "."
+    return interests_string
+  
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
