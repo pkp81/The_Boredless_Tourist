b411560e65d1c408dba4f488497b567eded3afd0
codecademy <ccuser@codecademy.com>
2019-06-05 20:49:09 +0000

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "So Paulo, Brazil", "Cairo, Egypt"]
-test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 
 def get_destination_index(destination):
   destination_index = destinations.index(destination)
@@ -14,3 +14,29 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 print("Index of Test Traveler: " + str(test_destination_index))
+
+attractions = [[] for i in range(len(destinations))]
+print(attractions)
+
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return 
+  except ValueError:
+    return
+      
+add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]] )
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("So Paulo, Brazil", ["So Paulo Zoo", ["zoo"]])
+add_attraction("So Paulo, Brazil", ["Ptio do Colgio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print(attractions)
+  
